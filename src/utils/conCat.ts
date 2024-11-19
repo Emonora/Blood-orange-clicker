657b420f2a169b3851c0cc5325242d2d4d8752d6
Emonora <114265017+Emonora@users.noreply.github.com>
2024-11-19 17:21:04 -0600

    Added all numbers up to decillions

--- src/utils/conCat.ts
+++ src/utils/conCat.ts
@@ -1,5 +1,33 @@
 export function conCat(num: number): string {
-  if (num >= 1_000_000_000_000) {
+  if (num >= 1_000_000_000_000_000_000_000_000_000_000_000_000) {
+    const value = num / 1_000_000_000_000_000_000_000_000_000_000_000_000;
+    return value % 1 === 0 ? `${value}D` : value.toFixed(2) + "D";
+  }
+  if (num >= 1_000_000_000_000_000_000_000_000_000_000_000) {
+    const value = num / 1_000_000_000_000_000_000_000_000_000_000_000;
+    return value % 1 === 0 ? `${value}N` : value.toFixed(2) + "N";
+  }
+  else if (num >= 1_000_000_000_000_000_000_000_000_000_000) {
+    const value = num / 1_000_000_000_000_000_000_000_000_000_000;
+    return value % 1 === 0 ? `${value}O` : value.toFixed(2) + "O";
+  }
+  else if (num >= 1_000_000_000_000_000_000_000_000_000) {
+    const value = num / 1_000_000_000_000_000_000_000_000_000;
+    return value % 1 === 0 ? `${value}SE` : value.toFixed(2) + "SE";
+  }
+  else if (num >= 1_000_000_000_000_000_000_000_000) {
+    const value = num / 1_000_000_000_000_000_000_000_000;
+    return value % 1 === 0 ? `${value}S` : value.toFixed(2) + "S";
+  }
+  else if (num >= 1_000_000_000_000_000_000) {
+    const value = num / 1_000_000_000_000_000_000;
+    return value % 1 === 0 ? `${value}QI` : value.toFixed(2) + "QI";
+  }
+  else if (num >= 1_000_000_000_000_000) {
+    const value = num / 1_000_000_000_000_000;
+    return value % 1 === 0 ? `${value}Q` : value.toFixed(2) + "Q";
+  }
+  else if (num >= 1_000_000_000_000) {
     const value = num / 1_000_000_000_000;
     return value % 1 === 0 ? `${value}T` : value.toFixed(2) + "T";
   }
