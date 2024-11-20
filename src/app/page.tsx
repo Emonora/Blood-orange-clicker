982f2f8055fd9411eb323f44430d97f52e543ed5
Emonora <114265017+Emonora@users.noreply.github.com>
2024-11-20 18:39:07 +0000

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,19 @@ import { useState, useEffect } from "react";
 import { getScore, setScor } from "~/utils/cookies/score";
 import Shop from "./_components/shop";
 import { getOwned } from "~/utils/cookies/getOwned";
+import { reset } from "~/utils/reset";
+import { winOrLose } from "~/utils/winOrLose";
+import { useToast } from "~/hooks/use-toast";
+import { conCat } from "~/utils/conCat";
 
 export default function HomePage() {
   const { toast } = useToast();
   const [score, setScore] = useState<number>(0);
 
   const handleClick = () => {
-    setScore(score + 1 + getOwned("cursor"));
-    setScor(score + 1 + getOwned("cursor"));
+    const newScore = score + 1 + getOwned("cursor");
+    setScore(newScore);
+    setScor(newScore);
   };
 
   const changeScore = (scor: number) => {
@@ -24,15 +29,32 @@ export default function HomePage() {
   }, []);
 
   useEffect(() => {
-    const update = setInterval(() => {
+    const update: any = setInterval(() => {
+      if (getScore() < 0) {
+        toast({
+          variant: "destructive",
+          title: "Game Over!",
+          description: "Your score is negative :(",
+        });
+        reset();
+        return () => clearInterval(update);
+      }
+
+      if (winOrLose(getScore()) === 1) {
+        toast({
+          title: "You Win",
+          description: "You've hit the score goal!",
+        });
+        reset();
+        return () => clearInterval(update);
+      }
+
       const updated =
         getOwned("tree") +
         getOwned("farm") +
         getOwned("shed") +
         getOwned("orchard");
-      console.log(updated);
       const oldScore = getScore();
-      console.log(oldScore);
       setScore(oldScore + updated);
       setScor(oldScore + updated);
     }, 1000);
@@ -41,8 +63,8 @@ export default function HomePage() {
   }, []);
 
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center bg-black text-white">
-      <p className="absolute left-0 top-0 p-4">Score: {score}</p>
+    <main className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-black text-white">
+      <p className="absolute left-0 top-0 p-4">Score: {conCat(score)}</p>
       <button
         onClick={handleClick}
         className="h-20 w-20 rounded-full bg-amber-500 shadow-lg hover:bg-amber-700 active:bg-amber-950"
