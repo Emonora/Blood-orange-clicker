cf362148be862f477fc0d55a2a862d4600175263
Emonora <114265017+Emonora@users.noreply.github.com>
2024-11-23 22:47:25 +0000

    made the code easier to read :3

--- src/utils/cookies/getCost.ts
+++ src/utils/cookies/getCost.ts
@@ -3,44 +3,21 @@ import Cookie from "js-cookie";
 export function getCost(name: string): number {
   const Cost = Cookie.get(`${name}_cost`);
   if (Cost === undefined) {
-    const List = ["cursor", "tree", "shed", "farm", "orchard", "greenhouse", "mine", "bank", "tower"];
-    for (const items of List) {
-      if (name == List[0]) {
-        setCost("cursor", 10);
-        return 10;
-      }
-      if (name == List[1]) {
-        setCost("tree", 100);
-        return 100;
-      }
-      if (name == List[2]) {
-        setCost("shed", 1000);
-        return 1000;
-      }
-      if (name == List[3]) {
-        setCost("farm", 10000);
-        return 10000;
-      }
-      if (name == List[4]) {
-        setCost("orchard", 100000);
-        return 100000;
-      }
-      if (name == List[5]) {
-        setCost("greenhouse", 1000000);
-        return 1000000;
-      }
-      if (name == List[6]) {
-        setCost("mine", 10000000);
-        return 10000000;
-      }
-      if (name == List[7]) {
-        setCost("bank", 100000000);
-        return 100000000;
-      }
-      if (name == List[8]) {
-        setCost("tower", 1000000000);
-        return 1000000000;
-      }
+    const costList: any = {
+        "cursor": 10,
+        "tree": 100,
+        "shed": 1000,
+        "farm": 10000,
+        "orchard": 100000,
+        "greenhouse": 1000000,
+        "mine": 10000000,
+        "bank": 100000000,
+        "tower": 1000000000
+    };
+
+    if (costList[name] !== undefined) {
+        setCost(name, costList[name]);
+        return costList[name];
     }
   }
 
