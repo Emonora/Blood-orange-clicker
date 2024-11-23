c5865674a7277d28321fc39b447ef103c1a42386
Emonora <114265017+Emonora@users.noreply.github.com>
2024-11-23 19:48:28 +0000

    added a cps counter as well

--- src/app/_components/owned.tsx
+++ src/app/_components/owned.tsx
@@ -1,36 +1,40 @@
+import { conCat } from "~/utils/conCat";
+import { getCPS } from "~/utils/cookies/getCps";
 import { getOwned } from "~/utils/cookies/getOwned";
 
 export default function Owned() {
     return (
-        <main className="text-sm overflow-auto max-h-full top-12 absolute left-0">
+        <main className="text-sm overflow-auto max-h-full top-12 gap-4 pt-2 pb-2 absolute left-0">
             <h1 className="pb-2 text-base">Owned Buildings:</h1>
             <p>
-                Owned Cursor(s): {getOwned("cursor")}
+                Owned Cursor(s): {conCat(getOwned("cursor"))}
             </p>
             <p>
-                Owned Tree(s): {getOwned("tree")}
+                Owned Tree(s): {conCat(getOwned("tree"))}
             </p>
             <p>
-                Owned Shed(s): {getOwned("shed")}
+                Owned Shed(s): {conCat(getOwned("shed"))}
             </p>
             <p>
-                Owned Farm(s): {getOwned("farm")}
+                Owned Farm(s): {conCat(getOwned("farm"))}
             </p>
             <p>
-                Owned Orchard(s): {getOwned("orchard")}
+                Owned Orchard(s): {conCat(getOwned("orchard"))}
             </p>
             <p>
-                Owned Greenhouse(s): {getOwned("greenhouse")}
+                Owned Greenhouse(s): {conCat(getOwned("greenhouse"))}
             </p>
             <p>
-                Owned Mine(s): {getOwned("mine")}
+                Owned Mine(s): {conCat(getOwned("mine"))}
             </p>
             <p>
-                Owned Bank(s): {getOwned("bank")}
+                Owned Bank(s): {conCat(getOwned("bank"))}
             </p>
             <p>
-                Owned Tower(s): {getOwned("tower")}
+                Owned Tower(s): {conCat(getOwned("tower"))}
             </p>
+
+            <h1>CPS: {conCat(getCPS())}</h1>
         </main>
     );
 }
