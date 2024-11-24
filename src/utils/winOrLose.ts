c7fea18838ac10ae4039eccc7d4b88894f31693a
Emonora <114265017+Emonora@users.noreply.github.com>
2024-11-24 15:38:33 -0600

    updated win lose conditions

--- src/utils/winOrLose.ts
+++ src/utils/winOrLose.ts
@@ -2,7 +2,7 @@ export function winOrLose(score: number): number {
   if (score < 0) {
     return 0;
   }
-  if (score > 1e27) {
+  if (score > 1_000_000_000_000_000_000_000_000_000_000_001) {
     return 1;
   }
   return 0.5;
